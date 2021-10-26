3530d52d5c48a41815af4d4a06f057f7744ee8f8
Bruno <bru_16@hotmail.com.ar>
2021-10-26 16:33:41 -0300

    custom hook for favorites gifs

--- src/components/FavoritesGifs/FavoritesGifs.js
+++ src/components/FavoritesGifs/FavoritesGifs.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import useUser from '../../hooks/useUser'
+import React from 'react'
+import useFavoritesGifs from '../../hooks/useFavoritesGifs'
 import Gifs from '../Gifs/Gifs'
-import { fetchGifsByIds } from '../../services/gifs'
 import Spinner from '../Spinner/Spinner'
 
 const FavoritesGifs = () => {
-
-    const [gifs, setGifs] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const { favs } = useUser();
-
-    useEffect(() => {
-        setLoading(true)
-        async function fetchGifs(favs) {
-            const fetchedGifs = await fetchGifsByIds({ favs });
-            setGifs(fetchedGifs);
-            setLoading(false);
-        }
-        if (favs.length > 0) fetchGifs(favs);
-    }, [favs])
+    const { gifs, loading, favs } = useFavoritesGifs()
 
     if (favs.length === 0) return <h4>Not favorites gifs found :(</h4>
     if (loading) return <Spinner />
